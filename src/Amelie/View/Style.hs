721cfc9c023955ca9ae0f9c7de94eefbb92199c6
Chris Done <chrisdone@gmail.com>
2011-06-09 01:14:06 +0200

    Default width 500px.

--- src/Amelie/View/Style.hs
+++ src/Amelie/View/Style.hs
@@ -192,6 +192,8 @@ home :: CSS Rule
 home = do
   rule "#home" wrap
   rule "#new" wrap
+  classRule "wrap" $ do
+    width "500px"
   
   where wrap = subRule ".amelie-wrap" $ do
                  width "50em"
